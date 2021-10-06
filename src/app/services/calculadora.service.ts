9cdabd883446d8ecf95b32369cb74e9c9eea3c06
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-06 16:48:56 -0300

    Fazendo requisição do tipo get para uma API

--- src/app/services/calculadora.service.ts
+++ src/app/services/calculadora.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
 @Injectable({
   providedIn: 'root'
 })
 export class CalculadoraService {
 
-  constructor() { }
+  // injetando o HttpClient como instância para poder usá-lo direto no construtor como sendo um parâmetro privado
+  constructor(private http: HttpClient) {
+    // fazendo requisição do tipo get para uma API
+    return this.http.get("https://st1.zoom.us/web_client/5g6glw/html/externalLinkPage.html?ref=https://pokeapi.co/api/v2/pokemon/ditto");
+    
+  }
+
+  public calcular(a:number, b:number, o:number) {
+
+  }
 }
