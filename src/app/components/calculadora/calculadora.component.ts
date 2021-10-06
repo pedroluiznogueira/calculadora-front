87104a9c6db5b0164c16678ec120f360e3d83d4c
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-06 15:33:52 -0300

    Criando a interface visual e relacionando com a classe CalculadoraComponent

--- src/app/components/calculadora/calculadora.component.ts
+++ src/app/components/calculadora/calculadora.component.ts
@@ -5,11 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './calculadora.component.html',
   styleUrls: ['./calculadora.component.css']
 })
-export class CalculadoraComponent implements OnInit {
+
+export class CalculadoraComponent implements OnInit {  
+    private a: number = 0;
+    private b: number = 0;
+    private operador: number = 0; 
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
